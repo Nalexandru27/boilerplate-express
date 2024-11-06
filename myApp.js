bd3cbae9e297aee07d808b983964f42e2d3c7cfe
Nastase Alexandru <nastasealexandru22@stud.ase.ro>
2024-11-06 14:11:27 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,9 +1,26 @@
+require('dotenv').config();
+
 let express = require('express');
 let app = express();
 
 console.log("Hello World");
 
+app.get("/", (req,res)=>{
+    //res.send("Hello Express");
+    absolutePath = __dirname +'/views/index.html';
+    res.sendFile(absolutePath);
+});
 
+app.use("/public",express.static(__dirname + "/public"));
+
+app.get("/json", (req, res)=>{
+    if(process.env.MESSAGE_STYLE == 'uppercase')
+        res.json({"message": "HELLO JSON"});
+    else
+    {
+        res.json({"message": "Hello json"});
+    }
+})
 
 
 
